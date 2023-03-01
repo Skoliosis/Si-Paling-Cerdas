b2491e1773bfd30bc408185007d2924025180969
zKevz <andriankevin69@gmail.com>
2023-03-01 21:23:53 +0700

    Linked raylib & simple window app

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <raylib.h>
 
-auto main() -> int
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 800
+#define APP_NAME "Si Paling Cerdas"
+
+int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Si Paling Cerdas");
+    SetTargetFPS(60);
+
+    auto length = MeasureText(APP_NAME, 20);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText(TextFormat("FPS: %d", GetFPS()), 0, 0, 30, GREEN);
+        DrawText(APP_NAME, WINDOW_WIDTH / 2 - length / 2, WINDOW_HEIGHT / 2, 20, WHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
